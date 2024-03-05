ae0b4ea89d44cde434c97e63a704bedad065ed37
Anton Kuts <anton.a.kuts@gmail.com>
2024-03-05 21:45:54 +0200

    made mistakes

--- Planing/components/events/EventItem.js
+++ Planing/components/events/EventItem.js
@@ -1,5 +1,8 @@
-import Link from "next/link";
 import style from "./EventItem.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem({ title, image, date, location, id }) {
   const humanReadebleDate = new Date(date).toLocaleDateString("en-US", {
@@ -19,14 +22,22 @@ function EventItem({ title, image, date, location, id }) {
         <div>
           <h2>{title}</h2>
           <div className={style.date}>
+            <DateIcon />
             <time>{humanReadebleDate}</time>
           </div>
           <div className={style.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={style.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            {" "}
+            <span>Explore Event</span>
+            <span className={style.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
